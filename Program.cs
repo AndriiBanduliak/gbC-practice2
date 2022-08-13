35be8719cd2c3810e510b2cded4ef5eec85f9429
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-13 11:14:05 +0300

    3rd task is done

--- Program.cs
+++ Program.cs
@@ -1,29 +1,13 @@
-﻿//Задача 13:Напишите программу, которая выводит третью (заданную) цифру заданного числа или сообщают, что третьей цыфры нет.
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является лиэтот день выходным.
 using static System.Console;
-int digitCount(int digit)//подсчет кол-ва цыфр
+bool isHoliday(int day) { return day>5 ? true : false; }
+int day;
+do
 {
-    int counter = 0;
-    while (digit != 0)
-    {
-        digit /= 10;
-        counter++;
-    }
-    return counter;
-}
+    Write("Enter a number for the day of the week: ");
+    day = int.Parse(Console.ReadLine());
+} while (day > 7||day<1);
 
-WriteLine("Enter the number");
-int digit = int.Parse(Console.ReadLine());
-WriteLine("What is the digit of this number?");
-int number = int.Parse(Console.ReadLine());
-
-
-if (digitCount(digit) <= -number || digitCount(digit) >= number)
-{
-    for (int i = 0; i < digitCount(digit); i++)
-    {
-        digit = (int)(digit / Math.Pow(10, (digitCount(digit) - number)));
-        digit %= 10;
-    }
-    Console.WriteLine($"{number} number is  {digit}");
-}
-else Console.WriteLine($"{number} number is not included {digit}");
+Write($"Is this day off? ");
+if (isHoliday(day)) Write("Yes");
+else Write("No.");
